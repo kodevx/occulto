824e24fa66d5b37691190853726c2eff2a965208
Rohith Nair <rohithnair2910@gmail.com>
2025-11-02 11:43:27 +0530

    Test Files Updates

--- src/utils/encryptData/encrypt.test.ts
+++ src/utils/encryptData/encrypt.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from 'vitest';
 import { getEncryptedText } from "./encryptData";
 import { SHIFT } from '../../constants/constants';
 
-const PLAIN_TEXT = 'Hello there Maria!';
-const CIPHER_TEXT = 'Khoor wkhuh Pduld!'
+const PLAIN_TEXT = 'Hello there John!';
+const CIPHER_TEXT = 'Khoor wkhuh Mrkq!'
 
 test(`(Hello there Maria!) Text Encryption (Small & Capital Letters) with Shift = ${SHIFT}`, () => {
     expect(
---
+++ src/utils/encryptData/index.ts
@@ -0,0 +1,1 @@
+export { getEncryptedText } from './encryptData';
