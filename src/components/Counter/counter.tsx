8ee8bf4797706c2218c7558fc13f163437091fab
Rohith Nair <rohithnair2910@gmail.com>
2025-11-06 15:06:10 +0530

    pseudo-state styles added for Buttons of Counter Component

--- src/components/Counter/counter.tsx
+++ src/components/Counter/counter.tsx
@@ -24,7 +24,7 @@ const Counter: React.FC<CounterProps>= (props) => {
                 <Button 
                     disabled={value === MIN_COUNT} 
                     onClick={handleDecrement}
-                    styles={'h-12 w-12 rounded-full bg-white active:bg-emerald-100 hover:bg-emerald-100 transition delay-50 duration-300 ease-out active:scale-50'}
+                    styles={'h-12 w-12 rounded-full bg-white active:bg-emerald-100 hover:bg-emerald-100 transition delay-50 duration-300 ease-out active:scale-50 disabled:cursor-not-allowed disabled:opacity-50'}
                 >
                     {'<'}
                 </Button>
@@ -32,7 +32,7 @@ const Counter: React.FC<CounterProps>= (props) => {
                 <Button 
                     disabled={value === MAX_COUNT} 
                     onClick={handleIncrement} 
-                    styles={'h-12 w-12 rounded-full bg-white active:bg-emerald-100 hover:bg-emerald-100 transition delay-50 duration-300 ease-out active:scale-50'}
+                    styles={'h-12 w-12 rounded-full bg-white active:bg-emerald-100 hover:bg-emerald-100 transition delay-50 duration-300 ease-out active:scale-50 disabled:cursor-not-allowed disabled:opacity-50'}
                 >
                     {'>'}
                 </Button>
